61ff02a2f2095771170dac6e405e6a01c8f31f9a
Jason Smythe <jason@smythemail.za.net>
2019-10-14 16:49:03 +0200

    Cleanup unused code + abstract use patronage calculate function to be used by other tests.

---
+++ test/helpers/index.js
@@ -0,0 +1,20 @@
+const { BN } = require('openzeppelin-test-helpers')
+
+const NUM_SECONDS_IN_YEAR = '31536000'
+
+module.exports = {
+  //patronage per token = price * amountOfTime * patronageNumerator/ patronageDenominator / 365 days;
+  multiPatronageCalculator: patronageDenominator => (timeInSeconds, tokenArray) => {
+    const totalPatronage = tokenArray.reduce(
+      (totalPatronage, token) =>
+        totalPatronage.add(
+          (new BN(token.price))
+            .mul(new BN(timeInSeconds))
+            .mul(new BN(token.patronageNumerator))
+            .div(new BN(patronageDenominator))
+            .div(new BN(NUM_SECONDS_IN_YEAR))
+        )
+      , new BN('0'));
+    return totalPatronage
+  }
+};
