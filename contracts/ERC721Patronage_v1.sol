895bf586f4804abe91432ca94ea26613cd5efb25
Jason Smythe <jason@smythemail.za.net>
2019-12-12 17:34:31 +0100

    Make amendments to the new steward

--- contracts/ERC721Patronage_v1.sol
+++ contracts/ERC721Patronage_v1.sol
@@ -22,22 +22,29 @@ contract ERC721Patronage_v1 is Initializable, ERC721, ERC721Enumerable, ERC721Me
     }
 
     function _isApprovedOrOwner(address spender, uint256 tokenId) internal view returns (bool) {
-        return (spender == steward || ERC721._isApprovedOrOwner(spender, tokenId));
+        return (
+          spender == steward 
+          /*
+          // NOTE: temporarily disabling sending of the tokens independently. A protective messure since it isn't clear to users how this function should work.
+          //       Will re-add once a mechanism is agreed on by the community.
+          || ERC721._isApprovedOrOwner(spender, tokenId)
+          */
+          );
     }
 
-    function transferFrom(address from, address to, uint256 tokenId) public {
-        if (msg.sender != steward) {
-            WildcardSteward_v1 stewardContract = WildcardSteward_v1(steward);
+    // function transferFrom(address from, address to, uint256 tokenId) public {
+    //     if (msg.sender != steward) {
+    //         WildcardSteward_v1 stewardContract = WildcardSteward_v1(steward);
 
-            // Calculate remaining deposit for the two addresses involved in transfer.
-            stewardContract._collectPatronagePatron(to);
-            stewardContract._collectPatronage(tokenId);
+    //         // Calculate remaining deposit for the two addresses involved in transfer.
+    //         stewardContract._collectPatronagePatron(to);
+    //         stewardContract._collectPatronage(tokenId);
 
-            // Do not allow someone to transfer a token if their deposit is Zero.
-            require(stewardContract.deposit(to) > 0, "Recipient needs to have a deposit.");
-            require(stewardContract.deposit(from) > 0, "Sender deposit has run out.");
-        }
+    //         // Do not allow someone to transfer a token if their deposit is Zero.
+    //         require(stewardContract.deposit(to) > 0, "Recipient needs to have a deposit.");
+    //         require(stewardContract.deposit(from) > 0, "Sender deposit has run out.");
+    //     }
 
-        ERC721.transferFrom(from, to, tokenId);
-    }
+    //     ERC721.transferFrom(from, to, tokenId);
+    // }
 }
