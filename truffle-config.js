96c022874cf1992b7a16d3fa630bdc13d34898d1
Jason Smythe <jason@smythemail.za.net>
2019-12-30 15:55:48 +0100

    Prettify all javascript!

--- truffle-config.js
+++ truffle-config.js
@@ -1,13 +1,13 @@
 const path = require("path");
-const HDWalletProvider = require('truffle-hdwallet-provider');
+const HDWalletProvider = require("truffle-hdwallet-provider");
 const {
   mnemonic,
   mainnetProviderUrl,
   rinkebyProviderUrl,
-  goerliProviderUrl,
-} = require("./secretsManager.js")
+  goerliProviderUrl
+} = require("./secretsManager.js");
 
-const blockchainNodeHost = process.env.BLOCKCHAIN_NODE_HOST || 'localhost'
+const blockchainNodeHost = process.env.BLOCKCHAIN_NODE_HOST || "localhost";
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -20,45 +20,45 @@ module.exports = {
       provider: new HDWalletProvider(mnemonic, mainnetProviderUrl, 0),
       gas: 4700000,
       gasPrice: 10000000000, // 10 gwei
-      skipDryRun: true,
+      skipDryRun: true
     },
     rinkeby: {
       network_id: 4,
       provider: new HDWalletProvider(mnemonic, rinkebyProviderUrl, 0),
       gas: 4700000,
       gasPrice: 10000000000, // 10 gwe
-      skipDryRun: true,
+      skipDryRun: true
     },
     goerli: {
       network_id: 5,
       provider: new HDWalletProvider(mnemonic, goerliProviderUrl, 0),
       // gas: 47000000,
       gasPrice: 10000000000, // 10 gwei
-      skipDryRun: true,
+      skipDryRun: true
     },
     development: {
-      host: blockchainNodeHost,     // Localhost (default: none)
-      port: 8545,            // Standard Ethereum port (default: none)
-      network_id: "*",       // Any network (default: none)
-      gasPrice: 1000000000, // 1 gwei
+      host: blockchainNodeHost, // Localhost (default: none)
+      port: 8545, // Standard Ethereum port (default: none)
+      network_id: "*", // Any network (default: none)
+      gasPrice: 1000000000 // 1 gwei
     },
     test: {
-      host: blockchainNodeHost,     // Localhost (default: none)
-      port: 8545,            // Standard Ethereum port (default: none)
-      network_id: "*",       // Any network (default: none)
-      gasPrice: 1000000000, // 1 gwei
-    },
+      host: blockchainNodeHost, // Localhost (default: none)
+      port: 8545, // Standard Ethereum port (default: none)
+      network_id: "*", // Any network (default: none)
+      gasPrice: 1000000000 // 1 gwei
+    }
   },
   mocha: {
-    reporter: 'eth-gas-reporter',
+    reporter: "eth-gas-reporter",
     reporterOptions: {
-      currency: 'USD',
-      gasPrice: 5, //in gwei
-    },
+      currency: "USD",
+      gasPrice: 5 //in gwei
+    }
   },
   compilers: {
     solc: {
-      version: '0.5.12',
+      version: "0.5.12",
       settings: {
         optimizer: {
           enabled: true,
