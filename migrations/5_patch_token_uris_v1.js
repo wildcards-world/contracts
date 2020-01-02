b8f3cb711dde3ad588e460e2d79eecc18c2db68a
Jason Smythe <jason@smythemail.za.net>
2020-01-02 12:56:20 +0100

    Make the deployment process more efficient (use a promise array) to send all transactions at once

--- migrations/5_patch_token_uris_v1.js
+++ migrations/5_patch_token_uris_v1.js
@@ -34,97 +34,97 @@ module.exports = function(deployer, networkName, accounts) {
 
     const uriFixerAddress = (await ERC721Patronage_v0.deployed()).address;
     const uriFixer = await URIFixer.at(uriFixerAddress);
-    await uriFixer.updateTokenUri(
-      0,
-      "https://ipfs.io/ipfs/QmcY1m22fmXR5uwELpiANNZTiji9jDaYt3iWmtJ2iyzTPd",
-      { from: accounts[0] }
-    );
-    await uriFixer.updateTokenUri(2, "https://ipfs.io/ipfs/", {
-      from: accounts[0]
-    });
-    await uriFixer.updateTokenUri(
-      1,
-      "https://ipfs.io/ipfs/QmWUh3RLNd4bR6Pf3YenXw9m9s1VKpqqSLWkDMo8gCyeMS",
-      {
-        from: accounts[0]
-      }
-    );
-    await uriFixer.updateTokenUri(
-      2,
-      "https://ipfs.io/ipfs/QmY2tMCbnrsZCuTWceHtWDf8C2frAg9MUPep9WQJwhEfFP",
-      {
-        from: accounts[0]
-      }
-    );
-    await uriFixer.updateTokenUri(
-      3,
-      "https://ipfs.io/ipfs/QmRn1XJ6QX51F6Sdz6xfxiUYyPpZkCFoi1AmhAJ4VgvjS5",
-      {
-        from: accounts[0]
-      }
-    );
-    await uriFixer.updateTokenUri(
-      4,
-      "https://ipfs.io/ipfs/QmcgjAKPP9Btibrd11pK9xW7t1tzgL45PaEqEqDwDp3qbX",
-      {
-        from: accounts[0]
-      }
-    );
-    await uriFixer.updateTokenUri(
-      5,
-      "https://ipfs.io/ipfs/QmbYDwg2DYm237xDzwBpEisLjx8XdxQAxtbE5myNhZ85Mg",
-      {
-        from: accounts[0]
-      }
-    );
-    await uriFixer.updateTokenUri(
-      6,
-      "https://ipfs.io/ipfs/QmPeztYoHZJXTVgdqNuyX6ZSyP6hcsG6rmDxCqvCpu6Pk2",
-      {
-        from: accounts[0]
-      }
-    );
-    await uriFixer.updateTokenUri(
-      7,
-      "https://ipfs.io/ipfs/Qmbbm8KVFnBXJirKfYZrjnmZ895hEcyw5t2ZjPERauTWUA",
-      {
-        from: accounts[0]
-      }
-    );
-    await uriFixer.updateTokenUri(
-      9,
-      "https://ipfs.io/ipfs/QmWW4mU9fXQSPuPpMXu6ZcNm17zjDonfbUn3FojTnddu1a",
-      {
-        from: accounts[0]
-      }
-    );
-    await uriFixer.updateTokenUri(
-      10,
-      "https://ipfs.io/ipfs/QmcsYFqJckrwM8n4xYVh3eRUA7exaiBWJGFETWBJj2cWmc",
-      {
-        from: accounts[0]
-      }
-    );
-    await uriFixer.updateTokenUri(
-      11,
-      "https://ipfs.io/ipfs/QmTuiKtFKqo8c67pGJTHUgedEBh9STbWvvPkeakSHVNGwc",
-      {
-        from: accounts[0]
-      }
-    );
-    await uriFixer.updateTokenUri(
-      12,
-      "https://ipfs.io/ipfs/QmTcNw6AhqjqiSrAGSAdSG7cmgmq9w33WNnjyi9A2xpBgp",
-      {
-        from: accounts[0]
-      }
-    );
-    await uriFixer.updateTokenUri(
-      42,
-      "https://ipfs.io/ipfs/QmTKddy2jARpWUWFATWcfiRrMNYCuiNPnVwYFTtJcJu6wv",
-      {
-        from: accounts[0]
-      }
-    );
+    await Promise.all([
+      uriFixer.updateTokenUri(
+        0,
+        "https://ipfs.io/ipfs/QmcY1m22fmXR5uwELpiANNZTiji9jDaYt3iWmtJ2iyzTPd",
+        { from: accounts[0] }
+      ),
+
+      uriFixer.updateTokenUri(
+        1,
+        "https://ipfs.io/ipfs/QmWUh3RLNd4bR6Pf3YenXw9m9s1VKpqqSLWkDMo8gCyeMS",
+        {
+          from: accounts[0]
+        }
+      ),
+      uriFixer.updateTokenUri(
+        2,
+        "https://ipfs.io/ipfs/QmY2tMCbnrsZCuTWceHtWDf8C2frAg9MUPep9WQJwhEfFP",
+        {
+          from: accounts[0]
+        }
+      ),
+      uriFixer.updateTokenUri(
+        3,
+        "https://ipfs.io/ipfs/QmRn1XJ6QX51F6Sdz6xfxiUYyPpZkCFoi1AmhAJ4VgvjS5",
+        {
+          from: accounts[0]
+        }
+      ),
+      uriFixer.updateTokenUri(
+        4,
+        "https://ipfs.io/ipfs/QmcgjAKPP9Btibrd11pK9xW7t1tzgL45PaEqEqDwDp3qbX",
+        {
+          from: accounts[0]
+        }
+      ),
+      uriFixer.updateTokenUri(
+        5,
+        "https://ipfs.io/ipfs/QmbYDwg2DYm237xDzwBpEisLjx8XdxQAxtbE5myNhZ85Mg",
+        {
+          from: accounts[0]
+        }
+      ),
+      uriFixer.updateTokenUri(
+        6,
+        "https://ipfs.io/ipfs/QmPeztYoHZJXTVgdqNuyX6ZSyP6hcsG6rmDxCqvCpu6Pk2",
+        {
+          from: accounts[0]
+        }
+      ),
+      uriFixer.updateTokenUri(
+        7,
+        "https://ipfs.io/ipfs/Qmbbm8KVFnBXJirKfYZrjnmZ895hEcyw5t2ZjPERauTWUA",
+        {
+          from: accounts[0]
+        }
+      ),
+      uriFixer.updateTokenUri(
+        9,
+        "https://ipfs.io/ipfs/QmWW4mU9fXQSPuPpMXu6ZcNm17zjDonfbUn3FojTnddu1a",
+        {
+          from: accounts[0]
+        }
+      ),
+      uriFixer.updateTokenUri(
+        10,
+        "https://ipfs.io/ipfs/QmcsYFqJckrwM8n4xYVh3eRUA7exaiBWJGFETWBJj2cWmc",
+        {
+          from: accounts[0]
+        }
+      ),
+      uriFixer.updateTokenUri(
+        11,
+        "https://ipfs.io/ipfs/QmTuiKtFKqo8c67pGJTHUgedEBh9STbWvvPkeakSHVNGwc",
+        {
+          from: accounts[0]
+        }
+      ),
+      uriFixer.updateTokenUri(
+        12,
+        "https://ipfs.io/ipfs/QmTcNw6AhqjqiSrAGSAdSG7cmgmq9w33WNnjyi9A2xpBgp",
+        {
+          from: accounts[0]
+        }
+      ),
+      uriFixer.updateTokenUri(
+        42,
+        "https://ipfs.io/ipfs/QmTKddy2jARpWUWFATWcfiRrMNYCuiNPnVwYFTtJcJu6wv",
+        {
+          from: accounts[0]
+        }
+      )
+    ]);
   });
 };
