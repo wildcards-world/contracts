d51e5b0a9a940d77031a36fd5fb936bf1677bdba
Jason Smythe <jason@smythemail.za.net>
2020-03-11 12:23:19 +0100

    Cleanup contracts and add mock function for testing ether transfers

---
+++ contracts/tests/SendBlockAttacker.sol
@@ -0,0 +1,23 @@
+pragma solidity 0.5.16;
+
+import "../WildcardSteward_v2.sol";
+
+contract SendBlockAttacker {
+    function buyOnBehalf(
+        WildcardSteward_v2 stewardAddress,
+        uint256 tokenId,
+        uint256 newPrice
+    ) public payable {
+        stewardAddress.buy.value(msg.value)(tokenId, newPrice);
+    }
+
+    function withdrawDeposit(WildcardSteward_v2 stewardAddress, uint256 amount)
+        public
+    {
+        stewardAddress.withdrawDeposit(amount);
+    }
+
+    function() external payable {
+        revert("I'm Malicious");
+    }
+}
