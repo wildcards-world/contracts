efb0e3bd8350b98376284dd423f7f80bdeca2412
Jason Smythe <jason@smythemail.za.net>
2020-04-17 21:15:45 +0200

    Some basic touchups to comments of the contracts

--- contracts/MintManager_v2.sol
+++ contracts/MintManager_v2.sol
@@ -3,11 +3,10 @@ pragma solidity 0.5.16;
 import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC721/ERC721.sol";
 import "./interfaces/IERC20Mintable.sol";
 
+
 contract MintManager_v2 is Initializable {
     using SafeMath for uint256;
 
-    //mapping(uint256 => IERC20Mintable) public erc20Minter; // set our erc20 token as default
-    // Things we need go here....
     address public admin;
     address public steward;
     IERC20Mintable public token;
@@ -16,19 +15,6 @@ contract MintManager_v2 is Initializable {
         require(msg.sender == steward, "Not steward");
         _;
     }
-
-    // modifier onlyAdmin() {
-    //     require(msg.sender == admin, "Not admin");
-    //     _;
-    // }
-
-    // modifier onlyAdminOrSteward() {
-    //     require(
-    //         msg.sender == admin || msg.sender == steward,
-    //         "Not admin or Steward"
-    //     );
-    //     _;
-    // }
 
     function initialize(address _admin, address _steward, address _token)
         public
