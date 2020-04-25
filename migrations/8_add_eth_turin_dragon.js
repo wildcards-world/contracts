a1cbc59fd03c8bc19c31a59efc8c392fc9ea7fcf
Jason Smythe <jason@smythemail.za.net>
2020-04-25 21:03:07 +0200

    add code to deploy eth turin dragon

--- migrations/8_add_eth_turin_dragon.js
+++ migrations/8_add_eth_turin_dragon.js
@@ -1,5 +1,10 @@
-const ERC721Patronage_v0 = artifacts.require("ERC721Patronage_v1");
-const WildcardSteward_v0 = artifacts.require("WildcardSteward_v2");
+const ERC721Patronage_v0 = artifacts.require("ERC721Patronage_v0");
+const WildcardSteward_v0 = artifacts.require("WildcardSteward_v0");
+const ERC721Patronage_v1 = artifacts.require("ERC721Patronage_v1");
+const WildcardSteward_v2 = artifacts.require("WildcardSteward_v2");
+
+const receiptGenerationRate = 11574074074074; // This is just less (rounded down) than one token a day (ie. 10^18 / 86400)
+const tokenId = "13";
 
 module.exports = function (deployer, networkName, accounts) {
   deployer.then(async () => {
@@ -8,31 +13,25 @@ module.exports = function (deployer, networkName, accounts) {
       return;
     }
 
-    const patronageToken = await ERC721Patronage_v0.deployed();
-    const steward = await WildcardSteward_v0.deployed();
+    const patronageToken_v0 = await ERC721Patronage_v0.deployed();
+    const steward_v0 = await WildcardSteward_v0.deployed();
+    const patronageToken = await ERC721Patronage_v1.at(
+      patronageToken_v0.address
+    );
+    const steward = await WildcardSteward_v2.at(steward_v0.address);
 
-    await Promise.all(
-      tokenData.map((token) => {
-        console.log("deploying token:", token.id);
-        return patronageToken.mintWithTokenURI(
-          steward.address,
-          token.id,
-          "https://wildcards.xyz/token/13",
-          { from: accounts[0] }
-        );
-      })
+    await patronageToken.mintWithTokenURI(
+      steward.address,
+      tokenId,
+      "https://wildcards.xyz/token/13",
+      { from: accounts[0] }
     );
 
-    let listOfTokenIds = tokenData.map((token) => token.id);
-    let benefactors = tokenData.map((token) => token.benefactor || accounts[0]);
-    let patronageNumerators = tokenData.map(
-      (token) => token.patronageNumerator
-    );
-    console.log("listing tokens with steward:", listOfTokenIds);
     await steward.listNewTokens(
-      listOfTokenIds,
-      benefactors,
-      patronageNumerators,
+      [tokenId],
+      [accounts[0]],
+      [600000000000], // Harberger tax rate of 60% per year
+      [receiptGenerationRate],
       { from: accounts[0] }
     );
   });
