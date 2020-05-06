3d154e278e95bc30497b6f3f059b5245fcf20f3a
Jason Smythe <jason@smythemail.za.net>
2020-05-06 15:26:41 +0200

    Deploy Glen

--- truffle-config.js
+++ truffle-config.js
@@ -4,7 +4,7 @@ const {
   mnemonic,
   mainnetProviderUrl,
   rinkebyProviderUrl,
-  goerliProviderUrl
+  goerliProviderUrl,
 } = require("./secretsManager.js");
 
 const blockchainNodeHost = process.env.BLOCKCHAIN_NODE_HOST || "localhost";
@@ -19,42 +19,42 @@ module.exports = {
       network_id: 1,
       provider: new HDWalletProvider(mnemonic, mainnetProviderUrl, 0),
       // gas: 4700000,
-      gasPrice: 10000000000, // 10 gwei
-      skipDryRun: true
+      gasPrice: 20000000000, // 20 gwei
+      skipDryRun: true,
     },
     rinkeby: {
       network_id: 4,
       provider: new HDWalletProvider(mnemonic, rinkebyProviderUrl, 0),
       gas: 4700000,
       gasPrice: 10000000000, // 10 gwe
-      skipDryRun: true
+      skipDryRun: true,
     },
     goerli: {
       network_id: 5,
       provider: new HDWalletProvider(mnemonic, goerliProviderUrl, 0),
       // gas: 47000000,
       gasPrice: 10000000000, // 10 gwei
-      skipDryRun: true
+      skipDryRun: true,
     },
     development: {
       host: blockchainNodeHost, // Localhost (default: none)
       port: 8545, // Standard Ethereum port (default: none)
       network_id: "*", // Any network (default: none)
-      gasPrice: 1000000000 // 1 gwei
+      gasPrice: 1000000000, // 1 gwei
     },
     test: {
       host: blockchainNodeHost, // Localhost (default: none)
       port: 8545, // Standard Ethereum port (default: none)
       network_id: "*", // Any network (default: none)
-      gasPrice: 1000000000 // 1 gwei
-    }
+      gasPrice: 1000000000, // 1 gwei
+    },
   },
   mocha: {
     reporter: "eth-gas-reporter",
     reporterOptions: {
       currency: "USD",
-      gasPrice: 5 //in gwei
-    }
+      gasPrice: 5, //in gwei
+    },
   },
   compilers: {
     solc: {
@@ -62,9 +62,9 @@ module.exports = {
       settings: {
         optimizer: {
           enabled: true,
-          runs: 200
-        }
-      }
-    }
-  }
+          runs: 200,
+        },
+      },
+    },
+  },
 };
