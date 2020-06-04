062ea67e5fc7dcd7d408f534fa525a8c8600a6e7
Jason Smythe <jason@smythemail.za.net>
2020-06-04 09:59:51 +0200

    Get migrations working for new contract (revert v2 to original v2)

---
+++ migrations/10_migration_test_for_v3.js
@@ -0,0 +1,77 @@
+const { ConfigManager, scripts } = require("@openzeppelin/cli");
+const { add, push, update } = scripts;
+
+const WildcardSteward_v0 = artifacts.require("WildcardSteward_v0");
+const WildcardSteward_v3 = artifacts.require("WildcardSteward_v3");
+
+const adminCheckerAddress = "0xDcC54cd9876230571bB68cC6d38ddeFc50095224";
+
+async function deploy(options) {
+  add({
+    contractsData: [{ name: "WildcardSteward_v3", alias: "WildcardSteward" }],
+  });
+
+  // Push implementation contracts to the network
+  await push({ ...options, force: true }); // I have to use force here because OpenZeppelin is being difficult :/ (and this is a hacky solution anyway...)
+  const tokensToUpgrade = [
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15,
+    16 /*17*/,
+    ,
+    42,
+  ];
+
+  // Update instance, adding +10 to value as part of the migration
+  let result = await update({
+    ...options,
+    contractAlias: "WildcardSteward",
+    methodName: "initialize",
+    methodArgs: [
+      tokensToUpgrade,
+      adminCheckerAddress,
+      0,
+      "100000000000000000000",
+    ],
+  });
+
+  console.log(result);
+
+  // Validate that the upgrade was successful:
+  const steward_v0 = await WildcardSteward_v0.deployed();
+  const steward = await WildcardSteward_v3.at(steward_v0.address);
+
+  for (let index = 0; index < tokensToUpgrade.length; index++) {
+    const token = tokensToUpgrade[index];
+    console.log(token);
+    // timeLastCollectedPatron[currentOwner] = now;
+  }
+}
+
+module.exports = function(deployer, networkName, accounts) {
+  deployer.then(async () => {
+    // Don't try to deploy/migrate the contracts for tests
+    if (networkName === "test") {
+      return;
+    }
+
+    const { network, txParams } = await ConfigManager.initNetworkConfiguration({
+      network: networkName,
+      from: accounts[0],
+    });
+    await deploy({ network, txParams }, accounts);
+  });
+};
