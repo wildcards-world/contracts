1c6fff0f78ba69d1844bb17901448428782cb7dd
Jason <Jason@wildcards.world>
2021-03-29 09:18:04 +0200

    Fix and write tests for edge cases

--- matic/test/steward_token_transfer.js
+++ matic/test/steward_token_transfer.js
@@ -1,4 +1,4 @@
-const { BN, ether, time } = require("@openzeppelin/test-helpers");
+const { BN, ether, time, expectRevert } = require("@openzeppelin/test-helpers");
 const {
   multiPatronageCalculator,
   setupTimeManager,
@@ -63,7 +63,7 @@ contract("WildcardSteward owed", (accounts) => {
       auctionEndPrice,
       auctionDuration,
       tokenDetails,
-      [accounts[2]]
+      [accounts[2], accounts[3], accounts[4]]
     );
     steward = result.steward;
     erc721 = result.erc721;
@@ -174,6 +174,188 @@ contract("WildcardSteward owed", (accounts) => {
       const deposit = await steward.deposit(accounts[3]);
 
       assert.equal(deposit.toString(), depositAfterSend.toString(), "another");
+    });
+  });
+  describe("steward: basic token send (error cases)", async () => {
+    // let usersDepositBeforeTransfer,
+    //   totalPatronOwnedTokenCost,
+    //   yearTimePatronagDenominator;
+    const testTokenId1 = tokenDetails[0].token;
+    const testTokenId2 = tokenDetails[1].token;
+    const priceAfterSend = ether("2");
+    const depositAfterSend = ether("1.5");
+    const priceOfToken1 = ether("1");
+    const priceOfToken2 = ether("2");
+
+    beforeEach(async () => {
+      const depositFor10MinForclosureToken1 = patronageCalculator("601", [
+        {
+          patronageNumerator: patronageNumerator,
+          price: priceOfToken1.toString(),
+        },
+      ]);
+      const depositFor10MinForclosureToken2 = patronageCalculator("600", [
+        {
+          patronageNumerator: patronageNumerator,
+          price: priceOfToken2.toString(),
+        },
+      ]);
+      //Buying 1st token and setting selling price to 1 eth. With 1 eth deposit.
+      let tx1 = await txTimestamp(
+        steward.buyAuction(
+          testTokenId1,
+          priceOfToken1,
+          50000,
+          depositFor10MinForclosureToken1,
+          {
+            from: accounts[2],
+          }
+        )
+      );
+
+      let tx2 = await txTimestamp(
+        steward.buyAuction(
+          testTokenId2,
+          priceOfToken2,
+          50000,
+          depositFor10MinForclosureToken2,
+          {
+            from: accounts[2],
+          }
+        )
+      );
+
+      usersDepositBeforeTransfer = await steward.deposit.call(accounts[2]);
+      totalPatronOwnedTokenCost = await steward.totalPatronOwnedTokenCost.call(
+        accounts[2]
+      );
+      yearTimePatronagDenominator = await steward.yearTimePatronagDenominator.call();
+    });
+
+    it("should allow transfer right up till the time of foreclosure", async () => {
+      await setNextTxTimestamp(time.duration.minutes(10).sub(new BN(1)));
+
+      await steward.transferWithDeposit(
+        accounts[3],
+        testTokenId1,
+        priceAfterSend,
+        depositAfterSend,
+        {
+          from: accounts[2],
+        }
+      );
+
+      const newOwner = await erc721.ownerOf(testTokenId1);
+
+      assert.equal(
+        newOwner,
+        accounts[3],
+        "Wrong owner of token after transfer"
+      );
+    });
+
+    it("should not allow the user to transfer a token if they are meant to be foreclosed", async () => {
+      await setNextTxTimestamp(time.duration.minutes(10));
+
+      await expectRevert(
+        steward.transferWithDeposit(
+          accounts[3],
+          testTokenId1,
+          priceAfterSend,
+          depositAfterSend,
+          {
+            from: accounts[2],
+          }
+        ),
+        "no deposit existing tokens"
+      );
+    });
+    it("shouldn't allow another user to transfer other tokens", async () => {
+      await expectRevert(
+        steward.transferWithDeposit(
+          accounts[3],
+          testTokenId1,
+          priceAfterSend,
+          depositAfterSend,
+          {
+            from: accounts[4],
+          }
+        ),
+        "not owner"
+      );
+    });
+    it("shouldn't allow a foreclosed user to recieve tokens", async () => {
+      // Make accounts[3] foreclosed
+      steward.buy(
+        testTokenId2,
+        priceOfToken2,
+        priceOfToken2,
+        50000,
+        priceOfToken2.add(new BN("5")),
+        {
+          from: accounts[3],
+        }
+      );
+      await setNextTxTimestamp(time.duration.minutes(10)); // enough time for account[3] to foreclose
+
+      await expectRevert(
+        steward.transferWithDeposit(
+          accounts[3],
+          testTokenId1,
+          priceAfterSend,
+          depositAfterSend,
+          {
+            from: accounts[2],
+          }
+        ),
+        "no deposit existing tokens"
+      );
+    });
+    it("deposit of sent token should be at least 1 week", async () => {
+      const depositFor1WeekMinus1Second = patronageCalculator(
+        time.duration.weeks("1").sub(new BN(1)),
+        [
+          {
+            patronageNumerator: patronageNumerator,
+            price: priceAfterSend.toString(),
+          },
+        ]
+      );
+      const depositFor1Week = patronageCalculator(time.duration.weeks("1"), [
+        {
+          patronageNumerator: patronageNumerator,
+          price: priceAfterSend.toString(),
+        },
+      ]);
+
+      await expectRevert(
+        steward.transferWithDeposit(
+          accounts[3],
+          testTokenId1,
+          priceAfterSend,
+          depositFor1WeekMinus1Second,
+          {
+            from: accounts[2],
+          }
+        ),
+        "Deposit less than 1 week"
+      );
+      await steward.transferWithDeposit(
+        accounts[3],
+        testTokenId1,
+        priceAfterSend,
+        depositFor1Week,
+        {
+          from: accounts[2],
+        }
+      );
+
+      const newOwner = await erc721.ownerOf(testTokenId1);
+      assert.equal(
+        newOwner,
+        accounts[3],
+        "Wrong owner of token after transfer"
+      );
     });
   });
 });
